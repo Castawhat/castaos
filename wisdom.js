42a62740dba719564ba38e5aa94564675e7e7c07
Josh <castawaypalmtree@gmail.com>
2025-11-12 08:02:54 +1300

    ge

--- wisdom.js
+++ wisdom.js
@@ -9,6 +9,30 @@ const quotes = [
 "I love Animal Crossing",
 "If theres a quizlet, theres an Excellence",
 "The calculator said 2+2=5 so who am I to argue?",
+"I want to cheese.",
+"I dont sleep much thats crazy howd you know that",
+"Push to prod, it works on my machine.",
+"Figma is life.",
+"Sometimes headache. Sometimes tired. Sometimes both. Always slitro.",
+"I am not foxboy that is someone else.",
+"This is why I use a mac",
+"This code works because I believe in magic.",
+"Why would I test it? I wrote it.",
+"Hey chatGPT why is my arduino smoking",
+"If you ask me to write this website in react, i will react violently.",
+"somebody stole my car radio",
+"somebody stole my code and put it on github.",
+"who needs documentation when you have no documentation?",
+"hey chatGPT why is my mac becoming a space heater",
+"just because it works doesnt mean its right.",
+"Im not procrastinating, Im doing side quests.",
+"when in doubt, just remove some comments.",
+// "Debugging: Being the detective in a crime movie where you
+// "re also the"
+"AAAAAAH JAVASCRIPT AAAAAAH",
+"Code never lies, comments sometimes do.",
+"To understand what recursion is, you must first understand recursion.",
+"I will repeat myself again, i am not foxboy.",
 ];
 
 function showRandomQuote() {
